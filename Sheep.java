dbc65916be6a6e60b5f0116e67be4009287f7a85
Kristian G. Madsen <kgm@kgm.cc>
2013-08-07 16:04:40 +0200

    Sheep can now coexist w/Plant on a point in the pasture.

--- Sheep.java
+++ Sheep.java
@@ -15,4 +15,18 @@ public class Sheep extends Animal {
         this.image = new ImageIcon("sheep.gif");
     }
 
+    @Override
+    /**
+     * Tests if this entity can be on the same position in the pasture
+     * as the given one.
+     */
+    public boolean isCompatible(Entity otherEntity) {
+        if( otherEntity instanceof Plant ) {
+            System.out.println( "Sheep stepped on plant");
+            return true;
+        }
+
+        // default not compatible
+        return false;
+    }
 }
